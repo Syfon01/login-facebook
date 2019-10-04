7554cb584ef3cdd093fa19d55287d285fd20a489
syfon01 <syfonisaac@gmail.com>
2019-10-04 16:14:13 +0100

    test

---
+++ build/precache-manifest.f1f359c4b7e97064073546b24b861701.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37da417d996dc89e9b68525388e01845",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1002f3d4d7f0098bd541",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2dbfd63e11bd5283ae00",
+    "url": "/static/css/main.9d460865.chunk.css"
+  },
+  {
+    "revision": "1002f3d4d7f0098bd541",
+    "url": "/static/js/2.6ebf5530.chunk.js"
+  },
+  {
+    "revision": "2dbfd63e11bd5283ae00",
+    "url": "/static/js/main.a3907ec8.chunk.js"
+  },
+  {
+    "revision": "65a81b1ca9a7620b8ceb",
+    "url": "/static/js/runtime-main.1dc4db23.js"
+  },
+  {
+    "revision": "fe5e02e890f5af7fc90c1a1f6611fea5",
+    "url": "/static/media/login-bg.fe5e02e8.jpg"
+  }
+]);
